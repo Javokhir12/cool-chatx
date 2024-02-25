63be63da2ee7ce3004869791f1361f25a4250e43
Javokhir Rajabboev <89512426+Javokhir12@users.noreply.github.com>
2024-02-25 13:45:40 +0500

    Setup Vitest and RTL (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  test: {
+    css: true,
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/__tests__/setupTests.ts'],
+  },
+});
